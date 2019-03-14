f37740cd7dbec9a9cc624d89ae6dd1f02772b91f
Mohammad M. Ahmad <mmahmad3@illinois.edu>
2019-03-14 16:40:35 -0500

    Setup POST method /forward API

--- app.py
+++ app.py
@@ -6,6 +6,13 @@ app = Flask(__name__)
 def index():
 	return "Hello World!"
 
+@app.route('/forward', methods=['POST'])
+def forward():
+	if(request.form.get('hop_count') > 0):
+		return "Will forward to EC2"
+	else:
+		return "Will forward to destination"
+
 if __name__ == '__main__':
 	app.debug = True
 	app.run(host='0.0.0.0')
