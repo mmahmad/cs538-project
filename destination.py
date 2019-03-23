d1cd2346e777b8aeda237f7e97d3b2e6fc2a50db
Mohammad M. Ahmad <mmahmad3@illinois.edu>
2019-03-23 15:09:00 -0500

    Destination Flask server

---
+++ destination.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask import request
+import time
+
+
+app = Flask(_name_)
+
+@app.route('/', methods=['GET','POST'])
+def hello():
+    print("->", request.form)
+    data = request.form
+    origin_ts = data['timestamp']
+    # need to convert to sec
+    time_elapsed = time.time()-float(origin_ts)
+    return str("time taken for request: ", time_elapsed)
+
+
+# run the app.
+if _name_ == "_main_":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    app.debug = True
+    app.run(host="localhost",port=8000)
