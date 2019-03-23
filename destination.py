d1f680a3ea95a556b5edde29c5bb56994d84062d
Mohammad M. Ahmad <mmahmad3@illinois.edu>
2019-03-23 15:30:15 -0500

    Run server on 0.0.0.0

--- destination.py
+++ destination.py
@@ -20,4 +20,4 @@ if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
     app.debug = True
-    app.run(host="localhost",port=8000)
+    app.run(host="0.0.0.0",port=8000)
