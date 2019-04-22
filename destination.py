868b2d05c10bd09acef999480f1f18f56a530f21
Mohammad M. Ahmad <mmahmad3@illinois.edu>
2019-04-22 16:02:45 -0500

    Remove all tests

--- destination.py
+++ destination.py
@@ -1,21 +1,8 @@
 from flask import Flask
 from flask import request
 import time
-# import signal
-# import sys
-import atexit
 
 app = Flask(__name__)
-
-routingTimes = []
-
-def exit_handler():
-        print('exit_handler called')
-        print("length of routingTimes:", len(routingTimes))
-        with open('times.txt', 'a+') as f:
-            for time_elapsed in routingTimes:
-                f.write(time_elapsed + "\n")
-        # sys.exit(0)
 
 @app.route('/', methods=['GET','POST'])
 def hello():
@@ -25,8 +12,8 @@ def hello():
     origin_ts = data['timestamp']
     # need to convert to sec
     time_elapsed = str(receivedTime-float(origin_ts))
-    routingTimes.append(time_elapsed)
-    print('length of routingTimes', len(routingTimes))
+    with open('times.txt', 'a+') as f:
+        f.write(time_elapsed + "\n")
 
     # print(str(time_elapsed))
     return ("time taken for request: " + str(time_elapsed))
@@ -36,10 +23,6 @@ def hello():
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
-    # signal.signal(signal.SIGINT, signal_handler)
-    # print('Press Ctrl+C to exit')
-    atexit.register(exit_handler)
     app.debug = True
     app.run(host="0.0.0.0",port=8000)
-    # signal.pause()
 
