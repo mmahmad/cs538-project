8129e028dc5488943c4e330f0e9b6b12485cc5da
Mohammad M. Ahmad <mmahmad3@illinois.edu>
2019-04-26 19:42:16 -0500

    Add more relay nodes and update README

--- app.py
+++ app.py
@@ -10,12 +10,74 @@ import click
 app = Flask(__name__)
 
 current_location = None # set in main() from cmd-line arg
+targets = {}
 
-IPs=[
-    'http://54.162.60.73:5000/forward', # east-1: N. Virginia
-    'http://18.218.55.58:5000/forward', # east-2: Ohio
-    'http://54.67.121.201:5000/forward', # west-1: N. California
-]
+# IPs=[
+#     'http://34.204.74.243:5000/forward', # east-1: N. Virginia
+#     'http://13.58.203.246:5000/forward', # east-2: Ohio
+#     'http://54.215.188.3:5000/forward', # west-1: N. California
+    
+#     'http://35.166.131.254:5000/forward', # Oregon
+#     'http://13.232.9.79:5000/forward', # Mumbai
+#     'http://13.125.255.243:5000/forward', # Seoul
+#     'http://54.255.237.155:5000/forward', # Singapore
+#     'http://54.252.137.172:5000/forward', # Sydney
+#     'http://13.114.92.252:5000/forward', # Tokyo
+#     'http://35.183.35.149:5000/forward', # Central Canada
+#     'http://18.197.144.127:5000/forward', # Frankfurt
+#     'http://34.244.33.202:5000/forward', # Ireland
+#     'http://35.178.94.52:5000/forward', # London
+#     'http://52.47.145.82:5000/forward', # Paris
+#     'http://13.53.38.55:5000/forward', # Stockholm
+# ]
+
+IpTable={
+    (37.9821704,-81.7696267): 'http://34.204.74.243:5000/forward', # east-1: N. Virginia
+    (40.3436035,-84.9124752): 'http://13.58.203.246:5000/forward', # east-2: Ohio
+    (38.8120855,-124.5556146): 'http://54.215.188.3:5000/forward', # west-1: N. California
+    
+    (44.1234992,-122.8263855): 'http://35.166.131.254:5000/forward', # Oregon
+    (19.0821978,72.7410999): 'http://13.232.9.79:5000/forward', # Mumbai
+    (37.5650172,126.8494656): 'http://13.125.255.243:5000/forward', # Seoul
+    (1.3139961,103.7041632): 'http://54.255.237.155:5000/forward', # Singapore
+    (-33.847927,150.6517866): 'http://54.252.137.172:5000/forward', # Sydney
+    (35.5040536,138.6486313): 'http://13.114.92.252:5000/forward', # Tokyo
+    (50.8325132,-130.1073912): 'http://35.183.35.149:5000/forward', # Central Canada
+    (50.1211277,8.4964811): 'http://18.197.144.127:5000/forward', # Frankfurt
+    (53.3942356,-10.1983315): 'http://34.244.33.202:5000/forward', # Ireland
+    (51.5285582,-0.2416811): 'http://35.178.94.52:5000/forward', # London
+    (48.8588377,2.2770203): 'http://52.47.145.82:5000/forward', # Paris
+    (59.3260668,17.8419713): 'http://13.53.38.55:5000/forward', # Stockholm
+}
+
+coordinateMapping = {
+    "nvirg": (37.9821704,-81.7696267),
+    "ncali": (38.8120855,-124.5556146),
+    "ohio": (40.3436035,-84.9124752),
+    "oregon": (44.1234992,-122.8263855),
+    "mumbai": (19.0821978,72.7410999),
+    "seoul": (37.5650172,126.8494656),
+    "singapore": (1.3139961,103.7041632),
+    "sydney": (-33.847927,150.6517866),
+    "tokyo": (35.5040536,138.6486313),
+    "canada": (50.8325132,-130.1073912),
+    "frankfurt": (50.1211277,8.4964811),
+    "ireland": (53.3942356,-10.1983315),
+    "london": (51.5285582,-0.2416811),
+    "paris": (48.8588377,2.2770203),
+    "stockholm": (59.3260668,17.8419713)
+}
+
+def getTargets():
+
+    # using the current_location, get coords of all other locations
+    coords = [v for k,v in coordinateMapping.items() if k != current_location]
+
+    # for each coords, add {coord: ip} in targets
+    for coord in coords:
+        # get ip from ipTable
+        ip = IpTable[coord]
+        targets[coord] = ip
 
 def deg2rad(degrees):
     return (degrees*math.pi)/180
@@ -30,12 +92,12 @@ def distance(p1,p2): #(latitude,longitude) tuples
 
 def picktarget(mycoordinate,destination,destip):
     # targets={(37.926868, -78.024902): IPs[0], (40.358615, -82.706838): IPs[1], (37.279518, -121.867905): IPs[2]} #map from coordinates to contact addresses
-    if current_location == 'ohio':
-        targets={(37.926868, -78.024902): IPs[0], (37.279518, -121.867905): IPs[2]} #map from coordinates to contact addresses
-    elif current_location == 'ncali':
-        targets={(37.926868, -78.024902): IPs[0], (40.358615, -82.706838): IPs[1]} #map from coordinates to contact addresses
-    elif current_location == 'nvirg':
-        targets={(40.358615, -82.706838): IPs[1], (37.279518, -121.867905): IPs[2]} #map from coordinates to contact addresses
+    # if current_location == 'ohio':
+    #     targets={(37.926868, -78.024902): IPs[0], (37.279518, -121.867905): IPs[2]} #map from coordinates to contact addresses
+    # elif current_location == 'ncali':
+    #     targets={(37.926868, -78.024902): IPs[0], (40.358615, -82.706838): IPs[1]} #map from coordinates to contact addresses
+    # elif current_location == 'nvirg':
+    #     targets={(40.358615, -82.706838): IPs[1], (37.279518, -121.867905): IPs[2]} #map from coordinates to contact addresses
 
     bestkey=mycoordinate
     for key in targets.keys():
@@ -108,5 +170,6 @@ if __name__ == '__main__':
         sys.exit()
 
     current_location = sys.argv[1]
+    getTargets() # set targets array
     app.debug = False
     app.run(host='0.0.0.0', port=5000)
